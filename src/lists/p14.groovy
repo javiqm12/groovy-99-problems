17f22a5b61d88202eb5b0d2d581a7484a98907d7
Javier Quintero <javiqm12@gmail.com>
2015-10-15 18:45:46 +0200

    New version using inject.

--- src/lists/p14.groovy
+++ src/lists/p14.groovy
@@ -9,8 +9,18 @@ List duplicate(List ls) {
     ls.collect { [it, it] }.flatten()
 }
 
+List duplicate2(List ls) {
+    ls.inject([]) { la, e -> la + [e, e] }
+}
+
 assert duplicate([1, 2, 3, 3, 4]) == [1, 1, 2, 2, 3, 3, 3, 3, 4, 4]
 assert duplicate([1, 1, 2]) == [1, 1, 1, 1, 2, 2]
 assert duplicate([5]) == [5, 5]
 assert duplicate([]) == []
 assert duplicate(['a']) == ['a', 'a']
+
+assert duplicate2([1, 2, 3, 3, 4]) == [1, 1, 2, 2, 3, 3, 3, 3, 4, 4]
+assert duplicate2([1, 1, 2]) == [1, 1, 1, 1, 2, 2]
+assert duplicate2([5]) == [5, 5]
+assert duplicate2([]) == []
+assert duplicate2(['a']) == ['a', 'a']
