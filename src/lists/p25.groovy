1b8666ceb905755f9500bfd28245c39936ccc2da
Javier Quintero <javiqm12@gmail.com>
2015-10-16 10:43:59 +0200

    Solved P25.

---
+++ src/lists/p25.groovy
@@ -0,0 +1,15 @@
+package lists
+
+//  P25 Generate a random permutation of the elements of a list.
+//      Hint: Use the solution of problem P23.
+//      Example:
+//      randomPermute([1, 2, 3, 4, 5])
+//      []
+
+import lists.p23
+
+List randomPermute(List ls) {
+    p23.randomSelect(ls.size(), ls)
+}
+
+assert randomPermute((1..49).toList()).size() == 49
