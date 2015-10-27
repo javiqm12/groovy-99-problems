b9b65a9e49b033aa7635cc18c57eb7f446fa6928
Javier Quintero <javiqm12@gmail.com>
2015-10-27 18:20:03 +0100

    Minor comment change.

--- src/arithmetic/p38.groovy
+++ src/arithmetic/p38.groovy
@@ -1,8 +1,8 @@
 package arithmetic
 
 //  P38 Compare the two methods of calculating Euler's totient function.
-//      Use the solutions of problems P34 and P37 to compare the algorithms.  Try
-//      to calculate phi(10090) as an example.
+//      Use the solutions of problems P34 and P37 to compare the algorithms.
+//      Try to calculate phi(10090) as an example.
 //  Example:
 //      totient(10)
 //      4
