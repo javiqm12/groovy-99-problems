8337fc65a94ff568da17b0acd7c6c670de3a4eab
Javier Quintero <javiqm12@gmail.com>
2015-10-27 08:00:07 +0100

    Solved P39.

---
+++ src/arithmetic/p39.groovy
@@ -0,0 +1,33 @@
+package arithmetic
+
+//  P39 Generate a list of primes in a given range.
+//  Example:
+//      primesInRenge(7, 31)
+//      [7, 11, 13, 17, 19, 23, 29, 31]
+
+def primesUpTo(bound) { 
+    def isPrime  = new BitSet(bound)
+    isPrime[0..1] = false
+    isPrime[2..bound] = true
+    (2..(Math.sqrt(bound))).each { pc ->
+        if (isPrime[pc]) {
+            ((pc**2)..bound).step(pc) { isPrime[it] = false }
+        }
+    }
+    (0..bound).findAll { isPrime[it] }
+}
+
+def primesInRange(a, b) {
+    primesUpTo(b).dropWhile { it < a }
+}
+
+assert primesInRange(7, 31) == [7, 11, 13, 17, 19, 23, 29, 31]
+
+def benchmark = { closure ->
+    start = System.currentTimeMillis()
+    closure.call()
+    System.currentTimeMillis() - start
+}
+
+println "Duration primesUpTo(10000): ${benchmark { primesUpTo(10000) }} ms"
+println "Duration primesUpTo(100000): ${benchmark { primesUpTo(100000) }} ms"
